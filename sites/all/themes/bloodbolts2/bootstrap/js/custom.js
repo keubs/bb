bd6e0629f78936843e0c40706f89fa425872acf4
Kevin Cook <kevinac4@gmail.com>
2016-04-07 22:54:34 +0000

    committing

--- sites/all/themes/bloodbolts2/bootstrap/js/custom.js
+++ sites/all/themes/bloodbolts2/bootstrap/js/custom.js
@@ -12,12 +12,6 @@
             columnWidth: '.grid-sizer',
         });
 
-        $('.view-details-products-and-pictures .view-content').masonry({
-            itemSelector: '.views-row',
-            percentPosition: true,
-            gutter: '.gutter-sizer',
-            columnWidth: '.grid-sizer',
-        });
 
         $('.view-journal .views-row').on('mouseenter', function(){
             $(this).find('.content-wrapper').addClass('active');
@@ -25,6 +19,17 @@
 
         $('.view-journal .views-row').on('mouseleave', function() {
            $(this).find('.content-wrapper').removeClass('active');
+        });
+
+        $('.view-details-products-and-pictures .view-content').masonry({
+            itemSelector: '.views-row',
+            percentPosition: true,
+            gutter: '.gutter-sizer',
+            columnWidth: '.grid-sizer',
+        });
+
+        $('.view-details-products-and-pictures .views-row.Product').on('mouseenter', function(){
+            $(this).find('.content-wrapper').addClass('active');
         });
     });
 
